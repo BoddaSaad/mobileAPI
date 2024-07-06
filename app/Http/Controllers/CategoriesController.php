3f693551bbb49740fbbae0165287c0a61485e407
BoddaSaad <abdos1166@gmail.com>
2024-07-06 21:03:56 +0300

    Offers Added to Category Items

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -17,6 +17,12 @@ class CategoriesController extends Controller
 
     public function show(Request $request){
         $items = Categories::with('items')->findOrFail($request->category);
-        return ItemsResource::collection($items->items);
+        $offers = Categories::with(['items' => function($q) {
+            $q->where('discount', '>', '0');
+        }])->find($request->category);
+        $itemCollection = [];
+        $itemCollection['items'] = ItemsResource::collection($items->items);
+        $itemCollection['offers'] = ItemsResource::collection($offers->items);
+        return $itemCollection;
     }
 }

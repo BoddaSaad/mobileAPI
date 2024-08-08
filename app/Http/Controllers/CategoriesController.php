71e83c22a09f1f1841d97b36e28d9fe60b25e78c
BoddaSaad <abdos1166@gmail.com>
2024-08-08 18:31:07 +0300

    Modifications on Single Category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Resources\CategoriesResource;
 use App\Http\Resources\ItemsResource;
 use App\Models\Categories;
+use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesController extends Controller
 {
@@ -16,9 +18,24 @@ class CategoriesController extends Controller
     }
 
     public function show(Request $request){
-        $items = Categories::with('items')->findOrFail($request->category);
-        $offers = Categories::with(['items' => function($q) {
-            $q->where('discount', '>', '0');
+        $query = $request->search ?? "";
+        $sort = $request->sort ?? "id";
+        $order = $request->order ?? "desc";
+        $items = Categories::with(['items' => function($q) use ($query, $sort, $order) {
+            $q->select('*', DB::raw('(items.discount / items.price) * 100 as offer'))
+                ->withAvg('ratings as rate', 'rating')
+                ->where("name", 'like', "%{$query}%")
+                ->orWhere("name_ar", 'like', "%{$query}%")
+                ->orderBy($sort, $order);
+        }])->findOrFail($request->category);
+        $offers = Categories::with(['items' => function($q) use ($query, $sort, $order) {
+            $q->select('*', DB::raw('(items.discount / items.price) * 100 as offer'))
+                ->where('discount', '>', '0')
+                ->withAvg('ratings as rate', 'rating')
+                ->where("name", 'like', "%{$query}%")
+                ->orWhere("name_ar", 'like', "%{$query}%")
+                ->where('discount', '>', '0')
+                ->orderBy($sort, $order);
         }])->find($request->category);
         $itemCollection = [];
         $itemCollection['items'] = ItemsResource::collection($items->items);

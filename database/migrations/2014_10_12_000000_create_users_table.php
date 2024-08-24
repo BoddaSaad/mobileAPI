a217d2766e22923e91394cb3f3f1d3813639cbee
BoddaSaad <abdos1166@gmail.com>
2024-08-24 07:57:02 +0300

    Default User Picture

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('photo')->nullable();
+            $table->string('photo')->default("uploads/default-pic.jpg");
             $table->integer('verifyCode')->nullable();
             $table->rememberToken();
             $table->timestamps();
